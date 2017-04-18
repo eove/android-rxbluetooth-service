a559f4f13c924fc9746e70d0ef34db3cd3c0cae7
Peio Roth <pierreroth64@gmail.com>
2017-04-18 10:56:54 +0200

    Service finds devices

--- App/service/src/main/java/fr/eove/android/bluetooth/service/BluetoothService.java
+++ App/service/src/main/java/fr/eove/android/bluetooth/service/BluetoothService.java
@@ -1,13 +1,29 @@
 package fr.eove.android.bluetooth.service;
 
 import android.app.Service;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.IBinder;
 import android.util.Log;
 
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
+
 public class BluetoothService extends Service {
+
     private static final String TAG = "BluetoothService";
+    private BluetoothAdapter bluetoothAdapter;
+    Disposable deviceDisposables;
+
     public BluetoothService() {
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
     }
 
     @Override
@@ -20,5 +36,36 @@ public class BluetoothService extends Service {
     public void onCreate() {
         super.onCreate();
         Log.d(TAG, "started service");
+        deviceDisposables = observeDevices().subscribe(new Consumer<BluetoothDevice>() {
+            @Override
+            public void accept(BluetoothDevice bluetoothDevice) throws Exception {
+                Log.d(TAG, "Found device: " + bluetoothDevice.getAddress());
+            }
+        });
+        startDiscovery();
+    }
+    public boolean startDiscovery() {
+        return bluetoothAdapter.startDiscovery();
+    }
+
+    public Observable<BluetoothDevice> observeDevices() {
+        final IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+
+        return Observable.create(new ObservableOnSubscribe<BluetoothDevice>() {
+            @Override
+            public void subscribe(final ObservableEmitter<BluetoothDevice> e) throws Exception {
+                final BroadcastReceiver receiver = new BroadcastReceiver() {
+                    @Override
+                    public void onReceive(Context context, Intent intent) {
+                        String action = intent.getAction();
+                        if (action.equals(BluetoothDevice.ACTION_FOUND)) {
+                            BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                            e.onNext(device);
+                        }
+                    }
+                };
+                BluetoothService.this.registerReceiver(receiver, filter);
+            }
+        });
     }
 }
