016f399c7d7da42c53df44546cff10fc3892f0dc
Peio Roth <pierreroth64@gmail.com>
2017-04-21 11:13:42 +0200

    Cleanups in service

--- service/src/main/java/fr/eove/android/bluetooth/service/BluetoothService.java
+++ service/src/main/java/fr/eove/android/bluetooth/service/BluetoothService.java
@@ -42,7 +42,9 @@ public class BluetoothService extends Service {
     private Subscription deviceConnectSubscription;
     private Subscription discoveryStartSubscription;
     private Subscription discoveryFinishSubscription;
+    private Subscription currentConnectionSubscription;
     private Map<String, BluetoothDevice> devices = new HashMap<>();
+    private BluetoothConnection currentConnection;
 
     public BluetoothService() {
 
@@ -108,12 +110,20 @@ public class BluetoothService extends Service {
     @Subscribe(threadMode = ThreadMode.ASYNC)
     public void onDeviceConnectRequest(DeviceConnectRequest event) {
         final String address = event.address;
-        Log.d(TAG, "requesting connection to " + address);
-        if ( !devices.containsKey(address)) {
+
+        if ( ! isDeviceKnown(address)) {
             Log.e(TAG, "unknown device with address: " + address);
             return;
         }
+        
+        installConnectionHandlerFor(address);
+    }
 
+    private boolean isDeviceKnown(String address) {
+        return devices.containsKey(address);
+    }
+
+    private void installConnectionHandlerFor(final String address) {
         deviceConnectSubscription = rxBluetooth.observeConnectDevice(devices.get(address), MY_UUID)
                 .subscribe(new Subscriber<BluetoothSocket>() {
                     @Override
@@ -129,19 +139,21 @@ public class BluetoothService extends Service {
                     @Override
                     public void onNext(BluetoothSocket bluetoothSocket) {
                         try {
-                            new BluetoothConnection(bluetoothSocket)
+                            currentConnection = new BluetoothConnection(bluetoothSocket);
+                            currentConnectionSubscription = currentConnection
                                     .observeBytesStream(40)
                                     .subscribe(new Action1<byte[]>() {
                                         @Override
                                         public void call(byte[] bytes) {
+                                            Log.d(TAG, "rec: " + toString(bytes));
+                                        }
+
+                                        private String toString(byte[] bytes) {
                                             Formatter formatter = new Formatter();
                                             for (byte b : bytes) {
                                                 formatter.format("%02x", b);
                                             }
-                                            String hex = formatter.toString();
-                                            Log.d("device", "rec: " + hex);
-
-                                            Log.d(TAG, "rec: " + hex);
+                                            return formatter.toString();
                                         }
                                     }, new Action1<Throwable>() {
                                         @Override
@@ -169,66 +181,8 @@ public class BluetoothService extends Service {
         unsubscribe(deviceSubscription);
         unsubscribe(discoveryStartSubscription);
         unsubscribe(discoveryFinishSubscription);
+        unsubscribe(currentConnectionSubscription);
     }
-
-//    private boolean isAlreadyConnectedTo(String address) {
-//        if (currentConnection != null) {
-//            if (currentConnection.isConnected() && currentConnection.getAddress().equals(address)) {
-//                return true;
-//            }
-//        }
-//        return false;
-//    }
-//
-//    private void connectionCleanUps() {
-//        if (connectedDeviceDisposable != null) {
-//            connectedDeviceDisposable.dispose();
-//        }
-//        if (currentConnection != null) {
-//            currentConnection.closeConnection();
-//        }
-//    }
-//
-//    @Subscribe(threadMode = ThreadMode.ASYNC)
-//    public void onDeviceConnectRequest(DeviceConnectRequest event){
-//        final String address = event.address;
-//        Log.d(TAG, "requesting connection to " + address);
-//
-//        if (isAlreadyConnectedTo(address)) {
-//            Log.d(TAG, "already connected to " + address);
-//            return;
-//        }
-//
-//        connectionCleanUps();
-//
-//        connectedDeviceDisposable = observeConnectedDevice(bluetoothAdapter.getRemoteDevice(address), MY_UUID)
-//                            .map(new Function<BluetoothSocket, BluetoothConnection>() {
-//                                @Override
-//                                public BluetoothConnection apply(BluetoothSocket bluetoothSocket) throws Exception {
-//                                    currentConnection = new BluetoothConnection(address, bluetoothSocket);
-//                                    return currentConnection;
-//                                }
-//                            })
-//                            .flatMap(new Function<BluetoothConnection, Observable<Byte>>() {
-//                                @Override
-//                                public Observable<Byte> apply(BluetoothConnection bluetoothConnection) throws Exception {
-//                                    return bluetoothConnection.observeByteStream();
-//                                }
-//                            })
-//                            .subscribe(new Consumer<Byte>() {
-//                                @Override
-//                                public void accept(Byte recv) throws Exception {
-//                                    Log.d(TAG, "recv: " + recv);
-//                                }
-//                            }, new Consumer<Throwable>() {
-//
-//                                @Override
-//                                public void accept(Throwable throwable) throws Exception {
-//                                    Log.e(TAG, "Oooops: " + throwable.toString());
-//                                }
-//                            });
-//        disposables.add(connectedDeviceDisposable);
-//    }
 
     @Subscribe(threadMode = ThreadMode.ASYNC)
     public void onDiscoveryStartRequest(DiscoveryStartRequest request){
@@ -242,102 +196,6 @@ public class BluetoothService extends Service {
         rxBluetooth.cancelDiscovery();
         EventBus.getDefault().post(new DiscoveryStatusEvent(DiscoveryStatus.CANCELLED));
     }
-
-//    private Observable<BluetoothDevice> observeDevices() {
-//        final IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-//
-//        return Observable.create(new ObservableOnSubscribe<BluetoothDevice>() {
-//            @Override
-//            public void subscribe(final ObservableEmitter<BluetoothDevice> e) throws Exception {
-//                final BroadcastReceiver receiver = new BroadcastReceiver() {
-//                    @Override
-//                    public void onReceive(Context context, Intent intent) {
-//                        String action = intent.getAction();
-//                        if (action.equals(BluetoothDevice.ACTION_FOUND)) {
-//                            BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-//                            e.onNext(device);
-//                        }
-//                    }
-//                };
-//                BluetoothService.this.registerReceiver(receiver, filter);
-//            }
-//        });
-//    }
-//
-//    public Observable<String> observeDiscovery() {
-//        final IntentFilter filter = new IntentFilter();
-//        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
-//        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
-//
-//        return Observable.create(new ObservableOnSubscribe<String>() {
-//            @Override
-//            public void subscribe(final ObservableEmitter<String> e) throws Exception {
-//                final BroadcastReceiver receiver = new BroadcastReceiver() {
-//                    @Override
-//                    public void onReceive(Context context, Intent intent) {
-//                        e.onNext(intent.getAction());
-//                    }
-//                };
-//                BluetoothService.this.registerReceiver(receiver, filter);
-//            }
-//        });
-//    }
-//
-//    public Observable<BluetoothDevice> observePairing() {
-//        final  IntentFilter filter = new IntentFilter();
-//        filter.addAction(BluetoothDevice.ACTION_PAIRING_REQUEST);
-//
-//        return Observable.create(new ObservableOnSubscribe<BluetoothDevice>() {
-//            @Override
-//            public void subscribe(final ObservableEmitter<BluetoothDevice> e) throws Exception {
-//                final BroadcastReceiver receiver = new BroadcastReceiver() {
-//                    @Override
-//                    public void onReceive(Context context, Intent intent) {
-//                        BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-//                        byte[] pinBytes = BuildConfig.PAIR_KEY.getBytes();
-//                        try {
-//                            device.setPin(pinBytes);
-//                            device.setPairingConfirmation(false);
-//                            device.createBond();
-//                            e.onNext(device);
-//                        } catch (Exception error) {
-//                            e.onError(error);
-//                        }
-//                    }
-//                };
-//                BluetoothService.this.registerReceiver(receiver, filter);
-//            }
-//        });
-//    }
-
-//    public Observable<BluetoothSocket> observeConnectedDevice(final BluetoothDevice bluetoothDevice, final UUID uuid) {
-//
-//        return Observable.create(new ObservableOnSubscribe<BluetoothSocket>() {
-//            @Override
-//            public void subscribe(ObservableEmitter<BluetoothSocket> e) throws Exception {
-//                BluetoothSocket bluetoothSocket;
-//                String devName = bluetoothDevice.getName();
-//                try {
-//                    bluetoothSocket = bluetoothDevice.createRfcommSocketToServiceRecord(uuid);
-//                    Log.d(TAG, "created socket");
-//                } catch (Exception createError) {
-//                    Log.e(TAG, "error when creating socket: " + createError.toString());
-//                    bluetoothSocket = bluetoothDevice.createInsecureRfcommSocketToServiceRecord(uuid);
-//                    Log.w(TAG, "created insecure socket");
-//                }
-//
-//                try {
-//                    Log.d(TAG, "connecting to " + devName + "...");
-//                    bluetoothSocket.connect();
-//                    Log.d(TAG, "connected to: " + devName);
-//                    e.onNext(bluetoothSocket);
-//                } catch (IOException error) {
-//                    Log.e(TAG, "failed to connect: " + error.toString());
-//                    e.onError(error);
-//                }
-//            }
-//        });
-//    }
 
     private static void unsubscribe(Subscription subscription) {
         if (subscription != null && !subscription.isUnsubscribed()) {
